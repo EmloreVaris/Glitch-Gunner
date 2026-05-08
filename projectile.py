8e73a2a2fb2a644d9d678cfa7769a90ea4cb33b4
EmloreVaris <108819739+EmloreVaris@users.noreply.github.com>
2026-05-08 14:46:35 -0500

    Prepare for database usage in projectile.py

--- projectile.py
+++ projectile.py
@@ -1,5 +1,11 @@
+import sqlite3
+
 class Projectile:
     def __init__(self, pid: int, vel: tuple[int, int], pos: tuple[int, int]) -> None:
         self.pid = pid
         self.vel = vel
         self.pos = pos
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM projectiles WHERE id = ?", (self.pid,))
+        data = cursor.fetchone()
